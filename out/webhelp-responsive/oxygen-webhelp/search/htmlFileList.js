9936136aded3757c610d9d7683b3c6134d30b846
kheath17 <kheath17@vt.edu>
2017-04-04 20:20:51 -0400

    out fixed

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,12 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "c-intro_armando.html";
+fl["1"]= "t-assemble.html";
+fl["2"]= "t-drink.html";
+fl["3"]= "t-eggs.html";
+fl["4"]= "t-hashbrowns.html";
+fl["5"]= "t-milk.html";
+fl["6"]= "t-peas.html";
+fl["7"]= "t-veggies.html";
+fl["8"]= "xt-guac.html";
+var doStem = false;searchLoaded = true;
